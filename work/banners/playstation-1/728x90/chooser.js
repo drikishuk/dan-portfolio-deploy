a1d97ece104b5334c2285936ed8e4db400714225
Ben Aldrich <benjaminaldrich9@gmail.com>
2026-03-30 15:07:56 +0100

    latest

--- work/banners/playstation-1/728x90/chooser.js
+++
@@ -1,9 +0,0 @@
-/* -- CHOOSE SHADOW -- */
-
-let shadow = "on";
-//let shadow = "off";
-
-
-/* -- CHOOSE CTA - If not using the Brand CTA remove it from the timeline in main.js -- */
-let brand_cta = "on";
-//let brand_cta = "off"; 
